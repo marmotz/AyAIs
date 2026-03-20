e9f8b0bda86a602de7f569f59ca3c6861bbadef7
Marmotz <contact@marmotz.dev>
2026-03-21 00:48:54 +0100

    Add changelog for version 0.4.0 with new features, improvements, and fixes

--- src/app/whatsnew/whatsnew.component.ts
+++ src/app/whatsnew/whatsnew.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChangesComponent } from '@app/whatsnew/changes/changes.component';
+import { ImprovementsComponent } from '@app/whatsnew/improvements/improvements.component';
 import { FixesComponent } from './fixes/fixes.component';
 import { NewFeaturesComponent } from './new-features/new-features.component';
 import { SpecialComponent } from './special/special.component';
@@ -6,7 +8,14 @@ import { VersionComponent } from './version/version.component';
 
 @Component({
   selector: 'app-whatsnew',
-  imports: [VersionComponent, NewFeaturesComponent, FixesComponent, SpecialComponent],
+  imports: [
+    VersionComponent,
+    NewFeaturesComponent,
+    FixesComponent,
+    SpecialComponent,
+    ChangesComponent,
+    ImprovementsComponent,
+  ],
   templateUrl: './whatsnew.component.html',
   styleUrl: './whatsnew.component.css',
 })
